6a4a5a85c41849549baecda9536826af599afb7b
wangshukai <736413509@qq.com>
2019-04-17 20:50:49 +0800

    v1.0 代码优化

--- app/src/main/java/com/saiyu/foreground/ui/activitys/MainActivity.java
+++ app/src/main/java/com/saiyu/foreground/ui/activitys/MainActivity.java
@@ -58,20 +58,12 @@ public class MainActivity extends BaseActivity implements BottomBar.OnTabSelecte
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /**
-         * 补充说明:
-         * 我们需要判断:该意图是打开一个新的任务,还是将后台的应用给提到前台来.
-         * 若是要将应用提到前台来直接将这个Activity结束掉,然后显示出来的Activity就是之前被最小化的Activity.
-         * 因为点击图标的意图会将新启动的Activity置于顶端,而顶端的下面的Activity就是之前被最小化的Activity.
-         * 此时结束掉新启动的Activity,就可以让之前被最小化的Activity 显示出来了.
-         *
-         * https://blog.csdn.net/busjb/article/details/40891239  感谢博主分享！
-         * */
 
-        //添加这个if语句
-        if((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0){
+        //添加这个if语句，防止oppo手机点击home键应用重新加载
+        if (!isTaskRoot()) {
             finish();
             return;
+
         }
 
         if (Build.VERSION.SDK_INT >= 21){
